fdc88c73b0f8af6da99667c29d73ba944291b8c2
RudraniJadaun <143861354+RudraniJadaun@users.noreply.github.com>
2024-11-21 12:27:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ function App() {
       <BrowserRouter>
       <Navbar/>
       <Routes>
-        <Route  path="/" element={<div className='bg-red-500 w-full h-[20vh] mt-10'><h1 >hello</h1></div>} />
+        <Route  path="/" element={<div className='bg-red-500 w-full h-[20vh] mt-10'><h1 >Go to about us</h1></div>} />
         <Route path="/About" element={<Header/>} />
-        <Route path="/tust" element={<div className='bg-green-500 w-full h-[20vh] mt-10'><h1 >hello</h1></div>} />
-        <Route path="/press" element={<div className='bg-gray-500 w-full h-[20vh] mt-10'><h1 >hello</h1></div>} />
-        <Route path="/contact" element={<div className=' bg-blue-600 w-full h-[20vh] mt-10'><h1 >hello</h1></div>} />
-        <Route path="/carrer" element={<div className='bg-yellow-200 w-full h-[20vh] mt-10'><h1 >hello</h1></div>} />
-        <Route path="/blog" element={<div className='bg-pink-400 w-full h-[20vh] mt-10'><h1 >hello</h1></div>} />
+        <Route path="/tust" element={<div className='bg-green-500 w-full h-[20vh] mt-10'><h1 >Go to about us</h1></div>} />
+        <Route path="/press" element={<div className='bg-gray-500 w-full h-[20vh] mt-10'><h1 >Go to about us</h1></div>} />
+        <Route path="/contact" element={<div className=' bg-blue-600 w-full h-[20vh] mt-10'><h1 >Go to about us</h1></div>} />
+        <Route path="/carrer" element={<div className='bg-yellow-200 w-full h-[20vh] mt-10'><h1 >Go to about us</h1></div>} />
+        <Route path="/blog" element={<div className='bg-pink-400 w-full h-[20vh] mt-10'><h1 >Go to about us</h1></div>} />
       </Routes>
       </BrowserRouter>
       
